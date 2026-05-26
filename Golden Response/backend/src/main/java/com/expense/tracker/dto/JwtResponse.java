975648d12e80550906422560e15133b4ecd0eada
Ayush Gupta <ayush.gupta1062@gmail.com>
2026-05-26 14:16:05 +0530

    Upload Golden Response/backend/src/main/java/com/expense/tracker/dto/JwtResponse.java

---
+++ Golden Response/backend/src/main/java/com/expense/tracker/dto/JwtResponse.java
@@ -0,0 +1,23 @@
+package com.expense.tracker.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class JwtResponse {
+    private String token;
+    private String type = "Bearer";
+    private Long id;
+    private String name;
+    private String email;
+
+    public JwtResponse(String token, Long id, String name, String email) {
+        this.token = token;
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+}
